29c5097ac7a256d9d198d32c76d405440c756598
kry-p <jhjung.dev@gmail.com>
2022-01-27 22:25:25 +0900

    Add TLS support

--- src/main.js
+++ src/main.js
@@ -14,15 +14,20 @@ import getReviewData from './modules/review';
 import compress from 'koa-compress';
 import zlib from 'zlib';
 
+import fs from 'fs';
+import https from 'https';
+
 require('dotenv').config();
 
 const app = new Koa();
 const router = new Router();
 
-const { PORT, MONGO_URI, BUILD_DIR } = process.env;
+const { PORT, MONGO_URI, BUILD_DIR, CERT_PATH } = process.env;
 const port = PORT || 4000;
 const buildDirectory =
   BUILD_DIR === undefined ? undefined : path.resolve(__dirname, BUILD_DIR);
+
+const serverCallback = app.callback();
 
 // get new review informations
 async function handleAsync() {
@@ -48,29 +53,65 @@ cron.schedule('*/10 * * * *', async () => {
   await handleAsync();
 });
 
-router.use('/api', api.routes());
+try {
+  router.use('/api', api.routes());
 
-app.use(router.routes()).use(router.allowedMethods());
-app.use(cors());
-app.use(
-  compress({
-    threshold: 8192,
-    flush: zlib.constants.Z_SYNC_FLUSH,
-  }),
-);
+  app.use(router.routes()).use(router.allowedMethods());
+  app.use(cors());
+  app.use(
+    compress({
+      threshold: 8192,
+      flush: zlib.constants.Z_SYNC_FLUSH,
+    }),
+  );
 
-// for stand-alone API server
-if (buildDirectory !== undefined) {
-  app.use(serve(buildDirectory));
-  app.use(async (ctx) => {
-    // not found, not started at /api
-    if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
-      // return index.html
-      await send(ctx, 'index.html', { root: buildDirectory });
-    }
+  // for stand-alone API server
+  if (buildDirectory !== undefined) {
+    app.use(serve(buildDirectory));
+    app.use(async (ctx) => {
+      // not found, not started at /api
+      if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+        // return index.html
+        await send(ctx, 'index.html', { root: buildDirectory });
+      }
+    });
+  }
+
+  // for https callback
+  if (CERT_PATH !== undefined) {
+    const config = {
+      domain: 'gamealive.xyz',
+      https: {
+        port: 443,
+        options: {
+          key: fs
+            .readFileSync(path.resolve(CERT_PATH, 'privkey.pem'), 'utf-8')
+            .toString(),
+          cert: fs
+            .readFileSync(path.resolve(CERT_PATH, 'fullchain.pem'), 'utf-8')
+            .toString(),
+        },
+      },
+    };
+    const httpsServer = https.createServer(
+      config.https.options,
+      serverCallback,
+    );
+
+    httpsServer.listen(config.https.port, function (err) {
+      if (err) {
+        logger.error('Server: https server cannot be established.');
+      } else {
+        logger.info(
+          `Server: Established - https://${config.domain}:${config.https.port}`,
+        );
+      }
+    });
+  }
+
+  app.listen(port, () => {
+    logger.info(`Server: Listening to port ${port}`);
   });
+} catch (e) {
+  logger.error('Server: Establishing instance failed.');
 }
-
-app.listen(port, () => {
-  logger.info(`Server: Listening to port ${port}`);
-});
